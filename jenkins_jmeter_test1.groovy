18272335284b7e5937468c6cca55f71b3e47e029
rasteppp <rasteppp@users.noreply.github.com>
2020-12-23 15:07:32 +0300

    jenkins_jmeter_test

---
+++ jenkins_jmeter_test1.groovy
@@ -0,0 +1,25 @@
+#!groovy
+// Check ubntnginx properties
+properties([disableConcurrentBuilds()])
+
+pipeline {
+    agent {
+        label 'master'
+    }
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
+        timestamps()
+    }
+    stages {
+        stage("First step") {
+            steps {
+                sh 'cd /home/rasteppp/jmeter/apache-jmeter-5.3'
+            }
+        }
+        stage("Second step") {
+            steps {
+                sh 'bin/jmeter -n -t pacingTimerExample/pacingTimerExampleUbuntu.jmx -l /mnt/d/PT/stud_projects/tests_JMeter/pacingTimerExample/log/result2.csv  -e -o /mnt/d/PT/stud_projects/tests_JMeter/pacingTimerExample/html/report/'
+            }
+        }
+    }
+}
